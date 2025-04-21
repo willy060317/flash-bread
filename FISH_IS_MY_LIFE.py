2a28fb430e8d4ddfb2bbcd3a78c568385a7a05bf
willy060317 <willy031712345678@gmail.com>
2025-04-22 04:40:43 +0900

    FISH_IS_MY_LIFE

--- FISH_IS_MY_LIFE.py
+++ FISH_IS_MY_LIFE.py
@@ -1,4 +1,14 @@
+붕어빵=[팥_붕어빵:10, 슈크림_붕어빵:10, 초코_붕어빵:10]
 while True:
     mode = input("원하는 모드를 선택하세요(주문, 관리자, 종료): ")
     if mode == "종료":
         break
+        print("시스템이 종료되었습니다")
+    elif mode == "주문": 
+        맛 = input("맛의 종류를 입력하세요")
+
+        수량  = input("수량을 입력하세요")
+    elif mode == "주문":
+        order_bread()
+    elif mode == "관리자":    
+        admin_mode()
